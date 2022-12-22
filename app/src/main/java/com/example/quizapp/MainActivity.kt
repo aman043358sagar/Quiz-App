b8992a830dfebdff442fc2e39b7aac949ae50c6e
Anurag Sagar <anurag.sagar@sensen.ai>
2022-12-23 02:07:32 +0530

    Quiz app

--- app/src/main/java/com/example/quizapp/MainActivity.kt
+++ app/src/main/java/com/example/quizapp/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.quizapp
 
+import android.graphics.drawable.GradientDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.quizapp.databinding.ActivityMainBinding
+import com.example.quizapp.databinding.FragmentMainBinding
 import com.example.quizapp.ui.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
